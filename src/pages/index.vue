faf5f9ce2b677c5347ddc20b7bf2c91dd52bb1b6
Anthony Ribeiro <anthony.ribeiro77@gmail.com>
2024-08-31 03:16:46 +0200

    add logo

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,10 @@
 <template>
   <v-container fill-height class="align-center justify-center">
 
+      <v-row class="justify-center mt-16 mb-16">
+        <v-img src="./../assets/logo.svg" max-width="300"></v-img>
+      </v-row>
+
       <RouterLink to="/oneplayer">
         <v-btn color="primary" class="mt-2" type="submit" block>One Player</v-btn>
       </RouterLink>
